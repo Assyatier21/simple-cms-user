86a4952c9d0c1966a3b412333bace56604c0651a
Muhammad Sholeh <muhammadsholeh.dev@gmail.com>
2022-12-22 21:29:39 +0700

    refactor: 💡 queries
    update query format

--- database/queries/article.go
+++ database/queries/article.go
@@ -1,7 +1,14 @@
 package database
 
 const (
-	GetArticles       = "SELECT a.id, a.title, a.slug, a.html_content, c.id, c.title , c.slug, a.created_at, a.updated_at FROM cms_article a JOIN cms_category c ON a.category_id = c.id ORDER BY a.id LIMIT $1 OFFSET $2"
-	GetArticleDetails = "SELECT a.id, a.title, a.slug, a.html_content, c.id, c.title , c.slug, a.created_at, a.updated_at FROM cms_article a JOIN cms_category c ON a.category_id = c.id WHERE a.id = $1"
-	GetMetaData       = "SELECT metadata FROM cms_article WHERE id = $1"
+	GetArticles = `SELECT a.id, a.title, a.slug, a.html_content, c.id, c.title , c.slug, a.created_at, a.updated_at 
+						FROM cms_article a JOIN cms_category c ON a.category_id = c.id 
+						ORDER BY a.id LIMIT $1 OFFSET $2`
+
+	GetArticleDetails = `SELECT a.id, a.title, a.slug, a.html_content, c.id, c.title , c.slug, a.created_at, a.updated_at 
+							FROM cms_article a JOIN cms_category c ON a.category_id = c.id 
+							WHERE a.id = $1`
+
+	GetMetaData = `SELECT metadata 
+						FROM cms_article WHERE id = $1`
 )
